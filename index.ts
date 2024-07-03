f9e1c0990888000fb4b2e622319ee69e2dc84e67
Isaac Roldán <isaac.roldan@shopify.com>
2024-07-03 12:53:04 +0200

    Use global_install as input

--- index.ts
+++ index.ts
@@ -31,7 +31,7 @@ try {
   };
 
   const buildScript = core.getInput('build_script');
-  const isGlobal = core.getInput('is_global') === 'true';
+  const isGlobal = core.getInput('global_install') === 'true';
   const packageOutputFilter = core
     .getInput('github_comment_included_packages')
     .split(',');
